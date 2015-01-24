f70c588367638727ebe00b6ae68de404075ca10e
erichoffert <ehoffert@gmail.com>
2015-01-24 10:57:31 -0500

    Update sstockjson.py

--- sstockjson.py
+++ sstockjson.py
@@ -59,8 +59,9 @@ print decoded['per_page']
 for x in range(0, decoded['per_page']):
   if (sys.argv[1] == 'videos'):
     myString = decoded['data'][x]['assets']['preview_mp4']
+    myString = re.search("(?P<url>https?://[^\s]+)", myString).group("url")
     #print "Video %s url = %s" % (x, decoded['data'][x]['assets']['preview_mp4'])
-    print "Video %s url = %s" % (x, re.search("(?P<url>https?://[^\s]+)", myString).group("url"))
+    print "Video %s url = %s" % (x, myString)
   elif (sys.argv[1] == 'audio'):
     myString = decoded['data'][x]['assets']['preview_mp3']
     #print "Audio %s url = %s" % (x, decoded['data'][x]['assets']['preview_mp3'])
