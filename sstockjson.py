7003b31992fe3c0f6ab96d1d6b9ac716f0649305
erichoffert <ehoffert@gmail.com>
2015-01-24 11:20:26 -0500

    Update sstockjson.py

--- sstockjson.py
+++ sstockjson.py
@@ -62,11 +62,11 @@ for x in range(0, decoded['per_page']):
   elif (sys.argv[1] == 'audio'):
     myString = decoded['data'][x]['assets']['preview_mp3']
     #print "Audio %s url = %s" % (x, decoded['data'][x]['assets']['preview_mp3'])
-    print "Audio %s url = %s" % (x, (x, re.findall('\S+http\S+',myString))
+    print "Audio %s url = %s" % (x, re.findall('\S+http\S+',myString))
   elif (sys.argv[1] == 'images'):
     myString = decoded['data'][x]['assets']['preview']
     #print "Image %s url = %s" % (x, decoded['data'][x]['assets']['preview'])
-    print "Image %s url = %s" % (x, (x, re.findall('\S+http\S+',myString))
+    print "Image %s url = %s" % (x, re.findall('\S+http\S+',myString))
     
     
 
