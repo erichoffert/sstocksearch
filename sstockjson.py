d22794f891858cadb4608bc7bbe37d819ab00eff
erichoffert <ehoffert@gmail.com>
2015-01-24 11:00:49 -0500

    Update sstockjson.py

--- sstockjson.py
+++ sstockjson.py
@@ -62,7 +62,7 @@ for x in range(0, decoded['per_page']):
   elif (sys.argv[1] == 'audio'):
     myString = decoded['data'][x]['assets']['preview_mp3']
     #print "Audio %s url = %s" % (x, decoded['data'][x]['assets']['preview_mp3'])
-    print "Audio %s url = %s" % (x, re.search("(?P<url>https?://[^\s]+)", myString).group("url"))
+    #print "Audio %s url = %s" % (x, re.search("(?P<url>https?://[^\s]+)", myString).group("url"))
   elif (sys.argv[1] == 'images'):
     myString = decoded['data'][x]['assets']['preview']
     #print "Image %s url = %s" % (x, decoded['data'][x]['assets']['preview'])
