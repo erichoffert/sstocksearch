2e6e98446ac078cfb0679257d9d87a96d6e085e7
erichoffert <ehoffert@gmail.com>
2015-01-25 18:16:16 -0500

    Update sstockjson.pl

--- sstockjson.pl
+++ sstockjson.pl
@@ -54,7 +54,7 @@ if ($resp->is_success) {
     foreach my $i (0..$per_page) {
      if ($s2 eq "images")
           print "Image url[",$i,"] = " . $decoded->{'data'}[$i]{'assets'}{'preview'}{'url'} . "\n";
-     elif ($s2 eq "audio")
+     elsif ($s2 eq "audio")
           print "Audio url[",$i,"] = " . $decoded->{'data'}[$i]{'assets'}{'preview_mp3'}{'url'} . "\n";
      else
           print "Video url[",$i,"] = " . $decoded->{'data'}[$i]{'assets'}{'preview_mp4'}{'url'} . "\n";
