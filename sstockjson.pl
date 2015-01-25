781da121e9065efe885e43464966fda151cf3371
erichoffert <ehoffert@gmail.com>
2015-01-25 16:05:13 -0500

    Update sstockjson.pl

--- sstockjson.pl
+++ sstockjson.pl
@@ -9,12 +9,12 @@ use Encode qw(decode encode);
 # syntax: sstockjson.pl  media-type keyword (search for micro stock media-type content based on keyword)
 
 print "starting shutterstock API";
-$s1="https://api.shutterstock.com/v2/";
-$s2=$ARGV[0];
-$s3="/search?per_page=1&query=";
-$s4=$ARGV[1];
-$s5="&view=full";
-$s6=$s1 + $s2 + $s3 + $s4 + $s5;
+my $s1="https://api.shutterstock.com/v2/";
+my $s2=$ARGV[0];
+my $s3="/search?per_page=1&query=";
+my $s4=$ARGV[1];
+my $s5="&view=full";
+my $s6=$s1 + $s2 + $s3 + $s4 + $s5;
 print "shutterstock api url $s6";
 
 # initialize the user agent for the http request to query for media content
@@ -37,14 +37,13 @@ my $resp = $ua->request($req);
 if ($resp->is_success) {
     my $message = $resp->decoded_content;
     print "Received reply: $message\n";
+    # encode the data as utf-8 and print it
+    my $messageutf8 = encode_utf8($message);
+    print "utf-8 encoded message $message\n";
 }
 else {
     print "HTTP GET error code: ", $resp->code, "\n";
     print "HTTP GET error message: ", $resp->message, "\n";
 }
 
-# encode the data as utf-8 and print it
 
-my $message-utf8 = encode_utf8($message);
-
-print "utf-8 encoded message $message\n";
