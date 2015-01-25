024b973abed93123654c9b1cd1cfa78cf9ba3799
erichoffert <ehoffert@gmail.com>
2015-01-25 18:32:29 -0500

    Update sstockjson.pl

--- sstockjson.pl
+++ sstockjson.pl
@@ -54,15 +54,14 @@ if ($resp->is_success) {
     foreach my $i (0..$per_page-1) {
      print "Video url $i = ";
      print $decoded->{'data'}[$i]{'assets'}{'preview_mp4'}{'url'}."\n";
-     if ($s2 eq 'videos') {
-         print "Video\n";
+     if ($s2 eq 'images') {
+          print "Image url[$i] = " . $decoded->{'data'}[$i]{'assets'}{'preview'}{'url'} . "\n";
      }
-     #if ($s2 eq "images")
-    #      print "Image url[$i] = " . $decoded->{'data'}[$i]{'assets'}{'preview'}{'url'} . "\n";
-     #elsif ($s2 eq "audio")
-     #     print "Audio url[$i] = " . $decoded->{'data'}[$i]{'assets'}{'preview_mp3'}{'url'} . "\n";
-     #else
-     #     print "Video url[$i] = " . $decoded->{'data'}[$i]{'assets'}{'preview_mp4'}{'url'} . "\n";
+     elsif ($s2 eq 'audio') {
+          print "Audio url[$i] = " . $decoded->{'data'}[$i]{'assets'}{'preview_mp3'}{'url'} . "\n";
+     }
+     else {
+          print "Video url[$i] = " . $decoded->{'data'}[$i]{'assets'}{'preview_mp4'}{'url'} . "\n";
     }
 }
 else {
