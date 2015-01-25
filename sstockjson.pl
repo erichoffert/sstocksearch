b364ff57ef7c6472cc2802c15f221cae26488232
erichoffert <ehoffert@gmail.com>
2015-01-25 16:37:38 -0500

    Update sstockjson.pl

--- sstockjson.pl
+++ sstockjson.pl
@@ -28,10 +28,10 @@ my $server_endpoint = $s6;
 # set the custom HTTP request header fields for content-type and basic authorization
 
 my $req = HTTP::Request->new(GET => $server_endpoint);
-#$req->header('content-type' => 'application/json');
-$req->header(Authorization => "Basic " . "MmY3MTAwNjdmNWIwNGY0NDNmZWE6NWRiM2QyODYxZDQ4ZDlmZmU0YzcxMzQ5NDQ4MjM5MDBlZmYzNDU2MA==");
+$req->header('content-type' => 'application/json');
+$req->header('authorization' => 'Basic ' . 'MmY3MTAwNjdmNWIwNGY0NDNmZWE6NWRiM2QyODYxZDQ4ZDlmZmU0YzcxMzQ5NDQ4MjM5MDBlZmYzNDU2MA==');
 
-print ("http basic authorization request\n", $req->header('Authorization'), "\n");
+print ("http basic authorization request\n", $req->header('authorization'), "\n");
 
 # make the http request with header fields and print the successful or failed response 
 
